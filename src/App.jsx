352423256f04c109161c548d3c39178f669ffde7
Eugénie Adelin <eugenie.aimee@hotmail.fr>
2026-05-02 17:31:24 +0200

    setup react app

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+import Footer from "./components/Footer";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <Navbar />
+
+      <div className="layout">
+        <Sidebar />
+
+        <main className="content">
+          <h1>Home Page</h1>
+        </main>
+      </div>
+
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
